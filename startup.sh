8b7ed5936005ce6c119f8437ef4be75a04b16c9e
ot-nemoto <nemoto@opentone.co.jp>
2020-09-03 07:58:06 +0000

    modify startup.sh
    - sendmailコマンド実行が遅い現象回避のため、`hosts`を更新する処理を追加

--- startup.sh
+++ startup.sh
@@ -3,6 +3,9 @@
 sed -i "s/^\(FEATURE.*\`msp',\s*\`\[\).*\(\].*\)/\1${MSP:-127.0.0.1}\2/g" /etc/mail/submit.mc
 make -C /etc/mail
 
+sed "s/^\(127.0.0.1\s\).*/\1localhost $(hostname) localhost.localdomain/g" /etc/hosts > hosts
+cat hosts | tee /etc/hosts
+
 /etc/init.d/httpd start
 
 tail -F /var/log/httpd/access_log
